54557b86ff1e4e3fd35217ee463b2f7e128ca60d
Imre Tabur <imre.tabur@eesti.ee>
2020-05-17 16:03:39 +0300

    Removed strange nonworking parts

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-/* jshint ignore:start */
 import servedjsGeo from 'servedjs-geo'
-/* jshint ignore:end */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -10,13 +8,10 @@ import filters from './filters'
 import { feature } from './directives'
 import version from './config/version'
 
-Vue.config.productionTip = process.env.NODE_ENV !== 'production'
+Vue.config.productionTip = false
 
-/* jshint ignore:start */
-const $log = jsdi.services.$log
 $log.setLevel($log.INFO)
 $log.info('App strted', version)
-/* jshint ignore:end */
 
 Vue.use(plugins)
 Vue.filter('inCurrency', filters.inCurrency)
